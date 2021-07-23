647e3aee2bc7fabbf805df40f4c6e2f22d5c39e4
Yesa <yesa@rumah.com>
2021-07-23 12:20:40 +0700

    Update the stack

--- stack_apigateway/stack_apigateway.py
+++ stack_apigateway/stack_apigateway.py
@@ -29,17 +29,37 @@ class StackApiGateway(cdk.Stack):
             function_arn=lambda_arn
         )
 
-        ## Require API Key to connect to REST API
-        rest_api_method_options = cdk_apigateway.MethodOptions(
-            api_key_required=True
-        )
-
-        ## Create REST API using Lambda Proxy Integration
+        ## Create REST API
         rest_api = cdk_apigateway.LambdaRestApi(self,
             id=generate_unique_id(),
             handler=lambda_function,
             rest_api_name=api_name,
-            default_method_options=rest_api_method_options
+            proxy=False,
+            deploy_options=cdk_apigateway.StageOptions(
+                stage_name="v1"
+            )
+        )
+
+        ## As the value of 'proxy' is False when creating the REST API, we need to create these by our own:
+        ## 1. Resource
+        ## 2. Method
+        resource_event = rest_api.root.add_resource(
+            path_part='events',
+            default_method_options=cdk_apigateway.MethodOptions(
+                api_key_required=True
+            )
+        )
+        resource_event_post_method = resource_event.add_method(
+            http_method='POST'
+        )
+
+        ## Add resource-based policy in Lambda for allowing invocation by resource_event_post_method
+        cdk_lambda.CfnPermission(self,
+            id=generate_unique_id(),
+            action="lambda:InvokeFunction",
+            function_name=lambda_function.function_name,
+            principal="apigateway.amazonaws.com",
+            source_arn=resource_event_post_method.method_arn
         )
 
         ## Create Usage Plan for the REST API
@@ -59,13 +79,4 @@ class StackApiGateway(cdk.Stack):
             id=generate_unique_id(),
             api_key=cdk_api_key,
             api_stages=[rest_api_usage_plan]
-        )
-
-        ## Add resource-based policy in Lambda for allowing invocation by REST API
-        cdk_lambda.CfnPermission(self,
-            id=generate_unique_id(),
-            action="lambda:InvokeFunction",
-            function_name=lambda_function.function_name,
-            principal="apigateway.amazonaws.com",
-            source_arn=rest_api.methods[0].method_arn
         )
